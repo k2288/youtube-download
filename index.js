24b3283409b79d43612cc4f77299e5a678a284b5
alireza jabbari <alirezajabarikopaie@gmail.com>
2024-07-21 17:09:59 +0330

    check node_modules

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ const PORT = 3000;
 // GET http://localhost:3000/download/book.png
 //
 app.get("/download", (req, res) => {
-    ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
-        .pipe(fs.createWriteStream('video.mp4'));
-    console.log('video.mp4')
-    res.download("video.mp4");
+    const stream=ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ', {
+
+    })
+    stream
+    .pipe(fs.createWriteStream('video.mp4'))
+    // .on("data",chunk=>{
+    //     console.log('downloaded', chunk.length);
+    // })
+    .on("finish", () => {
+        console.log('video downloaded successfully')
+    })
+    .on('error', (err) => {
+        console.error(`Error downloading video:`, err);
+    });
+    // console.log('video.mp4')
+    // res.download("video.mp4");
 
 });
 app.listen(PORT, () => console.log("Server listening to port " + PORT))
