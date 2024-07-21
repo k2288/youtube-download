3e4ebb526778518208bab80d410b6aff0f2e3095
alireza jabbari <alirezajabarikopaie@gmail.com>
2024-07-21 13:49:34 +0330

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const express = require("express")
+const app = express();
+const PORT = 3000;
+// 
+// GET http://localhost:3000/download/book.png
+//
+app.get("/download", (req, res) => {
+    ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
+        .pipe(fs.createWriteStream('video.mp4'));
+    console.log('video.mp4')
+    res.download("video.mp4");
+
+});
+app.listen(PORT, () => console.log("Server listening to port " + PORT))
